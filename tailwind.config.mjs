bc09ba8dc7179e2e247e07f0e7f1d4f08ee1bc57
sakuexe <saku.karttunen@gmail.com>
2024-02-09 14:33:39 +0200

    added fonts, colors and header sizes for tailwind use

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,38 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': {
+          '50': '#f7f6f9',
+          '100': '#efedf1',
+          '200': '#dad7e0',
+          '300': '#b9b4c5',
+          '400': '#928ba5',
+          '500': '#756d8a',
+          '600': '#5f5772',
+          '700': '#4e475d',
+          '800': '#433e4e',
+          '900': '#3b3644',
+          '950': '#242129',
+          DEFAULT: '#242129',
+        },
+      },
+      fontFamily: {
+        'sans': ['Montserrat', ...defaultTheme.fontFamily.sans],
+        'mono': ['"Fira Code"', 'monospace'],
+      },
+      fontSize: {
+        'header': ['clamp(4rem, 10vw, 7.5rem)', {
+          lineHeight: '1',
+        }],
+        'subheader': ['clamp(3.2rem, 8vw, 4rem)', {
+          lineHeight: '1',
+        }]
+      },
+    },
+  },
+  plugins: [],
 }
