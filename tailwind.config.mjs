6d00ad20bded3d57000ad4e64523c93f05305002
sakuexe <saku.karttunen@gmail.com>
2024-02-12 10:00:40 +0200

    made the skills section responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      maxWidth: {
+        'content': '100rem',
+      },
       colors: {
         'primary': {
           '50': '#f7f6f9',
